522553b8191f3ecc8bb31a1d6c12edb4f0d53a7f
NickF <lkbjhgfd@hotmail.com>
2021-04-05 15:43:04 -0400

    add learning

--- hallway.py
+++ hallway.py
@@ -6,6 +6,9 @@ interpolant_time = 0
 
 
 goal_condition = []
+goal_distance =dict()
+name_to_list = dict()
+parent_child = dict()
 
 def OneOf(selections):
     if len(selections) == 0:
@@ -16,7 +19,7 @@ def OneOf(selections):
 
 
 
-def add_if_not_subsumed(Ls, target):
+def add_if_not_subsumed(Ls, target, steps, parent, update_parent = True):
     i = 0
     while i < len(Ls):
         l = Ls[i]
@@ -31,23 +34,75 @@ def add_if_not_subsumed(Ls, target):
         i +=1
 
     Ls.append(target)
+    add_to_goal_distance(target, steps, parent, update_parent = update_parent)
     return Ls
+
+
+def add_to_goal_distance(Ls, steps, parent=None, update_parent= True, default_gap = 4):
+    key = str(Ls)
+    name_to_list[key] = Ls
+    goal_distance[key] = steps
+    if update_parent:
+        while parent is not None:
+            parent_key = str(parent)
+            parent_child[key]  = parent_key
+            goal_distance[parent_key] += (steps + default_gap)
+            parent = parent_child.get(parent_key, None)
+
+
+def remove_from_goal_distance(target):
+    key = str(target)
+    goal_distance[key] = 9999
+    parent = parent_child.get(key, None)
+    if parent is not None:
+        children = get_live_child(parent)
+        if len(children) == 0:
+            remove_from_goal_distance(parent)
+
+
+def get_live_child(target):
+    target_key = str(target)
+    return [child for child, parent in parent_child.items() if parent == target_key and goal_distance.get(child, 9999) < 9999]
+
+def get_one_with_the_least_step():
+    least_step = min(goal_distance.values())
+    min_size = 10000
+    target = None
+    v = min_size
+    for k in goal_condition[::-1]:
+        k = str(k)
+        v = goal_distance.get(k, -1)
+        if v == least_step:
+            size = len(name_to_list[k])
+            if size < min_size:
+                target = name_to_list[k]
+                min_size = size
+
+    return target, least_step
+
+def get_other_constraints(target):
+    rest = [Not(And(rst)) for rst in goal_condition if rst != target]
+    return And(rest)
+
+
+
 
 
 
 def main():
     start = time.time()
-    steps = 3
-    world_size = 6
+    steps = 4
+    world_size = 10
+    h = Hallway(world_size, steps, split=1, extended_goals=goal_condition)
     while True:
-        h = Hallway(world_size, steps, split=1, extended_goals=goal_condition)
         new_steps = h.interpolants()
         print(new_steps)
         if steps == new_steps:
             print("valid solution")
             break
         else:
-            steps = new_steps
+            h.recon_goals()
+            steps = steps
     end = time.time()
     print(start-end)
 
@@ -91,11 +146,58 @@ class Hallway():
         self.p1 = p1
         self.p1_states = self.get_all_state_variable(1)
         self.p0_states = self.get_all_state_variable(0)
+        i_1, i_n=  self.get_invaraints(split)
+        self.i_1 = i_1
+        self.i_n = i_n
+        self.solver.add(self.i_1)
         self.pn = pn
         self.solver.add(And(p1, pn))
-        self.goal = self.obtain_goals(goal, extended_goals)
-        self.solver.add(self.goal)
         self.simply_goal = goal
+        self.goal = self.obtain_goals(goal, extended_goals)
+        self.solver.push()
+        self.solver.add(self.goal)
+        self.inter_init = [self.set_init()]
+        self.other_init = True
+        self.goal_parent = self.simply_goal
+        goal_distance[str(self.simply_goal)] = 1
+
+    def get_invaraints(self, split):
+        return And([self.get_invaraint(i) for i in range(split)]), And(
+            [self.get_invaraint(i) for i in range(split, self.depth - 1)])
+
+    def get_invaraint(self, frame):
+        return And(OneOf(self.ats[frame]), OneOf(self.have_key[frame]))
+
+    def recon_goals(self):
+        goals = [self.simply_goal]
+        for e_g in goal_condition:
+            e_g = And(e_g)
+            for j in range(2, self.depth-1):
+                extended_goals = substitute(e_g, [ (p1_var, p0_var) for p1_var, p0_var in zip(self.p0_states, self.get_all_state_variable(self.depth-1))])
+                goals.append(extended_goals)
+        self.goal = Or(goals)
+        self.solver.pop()
+        self.solver.push()
+        self.solver.add(self.goal)
+        return
+
+    def recon_goals_min(self):
+        goals  = []
+        e_g, v = get_one_with_the_least_step()
+        self.goal_parent = e_g
+        e_g = And(e_g)
+        print("now consider goal {} with distance {}".format(e_g, v))
+        for j in range(2, self.depth - 1):
+            extended_goals = substitute(e_g, [(p1_var, p0_var) for p1_var, p0_var in
+                                              zip(self.p0_states, self.get_all_state_variable(self.depth - 1))])
+            goals.append(extended_goals)
+        self.goal = Or(goals)
+        self.solver.pop()
+        self.solver.push()
+        self.solver.add(self.goal)
+        self.other_init = get_other_constraints(self.goal_parent)
+        self.solver.add(self.other_init)
+        return
 
     def obtain_goals(self, original_goal, extended_goals= []):
         goals = [original_goal]
@@ -267,13 +369,8 @@ class Hallway():
             if (res == sat):
                 #analyze the model
                 m = self.solver.model()
+                self.find_minimized_solutions(m, steps, upper=5)
                 self.solver.pop()
-                pos, neg = mk_lit_spilit(m, self.p0_states)
-                self.solver.push()
-                L = minimize_L(self.solver, pos, neg)
-                self.solver.pop()
-                print(L)
-                goal_condition = add_if_not_subsumed(goal_condition, L)
                 return steps + self.depth
             else:
                 self.solver.pop()
@@ -284,18 +381,63 @@ class Hallway():
                 if sanity_check.check() == unsat:
                     print("okay")
                 '''
-                R = binary_interpolant(And(init, self.p1), And(self.pn, self.goal))
+                R = binary_interpolant(And(init, self.p1, self.other_init, self.i_1), And(self.pn, self.goal))
                 #print(R)
                 new_init = substitute(R, [ (p1_var, p0_var) for p1_var, p0_var in zip(self.p1_states, self.p0_states)])
-                #s = Solver()
-                #s.add(Not(Implies(new_init, init)))
-                #if (s.check() == unsat):
-                #    print("fix point, no more")
-                #    return -1
-                init = new_init
-                #print(init)
+                s = Solver()
+                s.add(Not(Implies(new_init, init)))
+                if (s.check() == unsat):
+                   print("fix point, no more")
+                   remove_from_goal_distance(self.goal_parent)
+                   return -1
+                #init = new_init
+                #print("new init from interpol: {}".format(init))
+                self.inter_init.append(new_init)
+                init = Or(self.inter_init)
                 steps +=1
                 print("progress")
+
+    def find_minimized_solutions(self, m, steps, upper=5):
+        global goal_condition
+        counter = 0
+        pos, neg = mk_lit_spilit(m, self.p0_states)
+        min_L = minimize_L(self.solver, pos, neg)
+        print(min_L)
+        goal_condition = add_if_not_subsumed(goal_condition, min_L, steps, self.goal_parent)
+        self.init_clean_up(And(min_L))
+        while counter < upper:
+            self.solver.add(Not(And(min_L)))
+            res = self.solver.check()
+            if (res == sat):
+                m = self.solver.model()
+                pos, neg = mk_lit_spilit(m, self.p0_states)
+                min_L = minimize_L(self.solver, pos, neg)
+                self.init_clean_up(And(min_L))
+                print(min_L)
+                goal_condition = add_if_not_subsumed(goal_condition, min_L, steps, self.goal_parent, update_parent=False)
+                counter+=1
+                continue
+            else:
+                return
+
+    def init_clean_up(self, L):
+        i = 0
+        check_solver = Solver()
+        check_solver.add(L)
+        while i < len(self.inter_init):
+            check_solver.push()
+            current = self.inter_init[i]
+            check_solver.add(current)
+            res = check_solver.check()
+            check_solver.pop()
+            if res == sat:
+                self.inter_init = self.inter_init[:i] + self.inter_init[i+1:]
+            else:
+                i+=1
+
+    def covered_goals(self, L):
+        
+
 
 def mk_lit(m, x):
     if is_true(m.eval(x)):
@@ -315,7 +457,10 @@ def mk_lit_spilit(m, L):
 
 
 
+
+
 def minimize_L(B, L, neg):
+    B.push()
     tick = 0
     changed = True
     B.add(And(neg))
@@ -332,7 +477,9 @@ def minimize_L(B, L, neg):
                 break
             if tick >= 100:
                 print("wow")
+                B.pop()
                 return L
+    B.pop()
     return L
 
 
